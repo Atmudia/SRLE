37c7f981e40270482ec7661837a38444a681bb2d
KomiksPL <Komikspl07@gmail.com>
2022-05-31 00:29:09 +0200

    UI updated

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,6 +1,9 @@
 ﻿using SRML;
+using SRML.Console;
 using SRML.SR;
 using SRML.SR.UI.Utils;
+using SRML.Utils;
+using UnityEngine;
 
 namespace SRLE
 {
@@ -8,15 +11,19 @@ namespace SRLE
     {
         public override void PreLoad()
         {
+            
             SRCallbacks.OnMainMenuLoaded += menu =>
             {
+                var c = PrefabUtils.CopyPrefab(SRObjects.Get<ExpoGameSelectUI>().gameObject);
                 var addMainMenuButton = MainMenuUtils.AddMainMenuButton(menu, "SRLE", () =>
                 {
-
+                    Object.Instantiate(c);
                 });
                 addMainMenuButton.name = "SRLEButton";
                 addMainMenuButton.transform.Find("Text").GetComponent<XlateText>().SetKey("b.srle");
                 addMainMenuButton.transform.SetSiblingIndex(4);
+                
+
             };
         }
     }
---
+++ SRObjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SRLE
+{
+    public class SRObjects
+    {
+        public static T Get<T>() where T : Object
+        {
+            foreach (var o in Resources.FindObjectsOfTypeAll<T>())
+            {
+                return o;
+            }
+
+            return null;
+        }
+    }
+}
