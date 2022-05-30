766893d92cc94460be4995cdfe6845c27a030425
KomiksPL <Komikspl07@gmail.com>
2022-05-31 00:58:35 +0200

    OnClick Function

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -4,6 +4,7 @@ using SRML.SR;
 using SRML.SR.UI.Utils;
 using SRML.Utils;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SRLE
 {
@@ -14,11 +15,15 @@ namespace SRLE
             
             SRCallbacks.OnMainMenuLoaded += menu =>
             {
+                
                 var c = PrefabUtils.CopyPrefab(SRObjects.Get<ExpoGameSelectUI>().gameObject);
                 var addMainMenuButton = MainMenuUtils.AddMainMenuButton(menu, "SRLE", () =>
                 {
-                    Object.Instantiate(c);
+                    /*Destroyer.Destroy(Object.FindObjectOfType<MainMenuUI>().gameObject, "SRLE.Button");
+                    menu.InstantiateAndWaitForDestroy(c);
+                    */
                 });
+                addMainMenuButton.GetComponent<Button>().onClick = menu.transform.Find("ExpoModePanel/PlayButton").GetComponent<Button>().onClick;
                 addMainMenuButton.name = "SRLEButton";
                 addMainMenuButton.transform.Find("Text").GetComponent<XlateText>().SetKey("b.srle");
                 addMainMenuButton.transform.SetSiblingIndex(4);
