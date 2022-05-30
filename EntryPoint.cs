08d04e765643611ac2ba1da58e1648980455b2bd
KomiksPL <Komikspl07@gmail.com>
2022-05-31 00:14:34 +0200

    Commit

---
+++ EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using SRML;
+using SRML.SR;
+using SRML.SR.UI.Utils;
+
+namespace SRLE
+{
+    public class EntryPoint : ModEntryPoint
+    {
+        public override void PreLoad()
+        {
+            SRCallbacks.OnMainMenuLoaded += menu =>
+            {
+                var addMainMenuButton = MainMenuUtils.AddMainMenuButton(menu, "SRLE", () =>
+                {
+
+                });
+                addMainMenuButton.name = "SRLEButton";
+                addMainMenuButton.transform.Find("Text").GetComponent<XlateText>().SetKey("b.srle");
+                addMainMenuButton.transform.SetSiblingIndex(4);
+            };
+        }
+    }
+}
