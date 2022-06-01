c710c76a65c15d6432b010067f6606edfc09249d
toc8730 <jayinalbertjohnson@gmail.com>
2022-06-01 16:55:27 -0400

    Added LevelsUI

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -5,17 +5,17 @@ using SRML.SR.UI.Utils;
 using SRML.Utils;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Reflection;
 namespace SRLE
 {
     public class EntryPoint : ModEntryPoint
     {
+        public static AssetBundle assetBundle => AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(typeof(EntryPoint), "srle_replaced"));
         public override void PreLoad()
         {
             
             SRCallbacks.OnMainMenuLoaded += menu =>
             {
-                
                 var c = PrefabUtils.CopyPrefab(SRObjects.Get<ExpoGameSelectUI>().gameObject);
                 var addMainMenuButton = MainMenuUtils.AddMainMenuButton(menu, "SRLE", () =>
                 {
@@ -23,12 +23,13 @@ namespace SRLE
                     menu.InstantiateAndWaitForDestroy(c);
                     */
                 });
-                addMainMenuButton.GetComponent<Button>().onClick = menu.transform.Find("ExpoModePanel/PlayButton").GetComponent<Button>().onClick;
+                addMainMenuButton.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    UnityEngine.Object.Instantiate(assetBundle.LoadAsset<GameObject>("LevelsUI")); // the name of the ui object in the assetbundle
+                });
                 addMainMenuButton.name = "SRLEButton";
                 addMainMenuButton.transform.Find("Text").GetComponent<XlateText>().SetKey("b.srle");
                 addMainMenuButton.transform.SetSiblingIndex(4);
-                
-
             };
         }
     }
